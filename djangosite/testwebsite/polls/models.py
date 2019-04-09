ad5078f585874e0c0aa254d73246f4155420907d
Jack Hales <halesyy@gmail.com>
2019-04-09 20:42:55 +1000

    Starting to understand holistic view of Django's MVC framework

--- djangosite/testwebsite/polls/models.py
+++ djangosite/testwebsite/polls/models.py
@@ -1,4 +1,11 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+from django.shortcuts import get_object_or_404
+
 
 """
 1. Cange your models (in models.py).
@@ -11,8 +18,25 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    # def exists(self, pk):
+    #     try:
+    #         self.objects.get(pk=pk)
+    #         return True
+    #     except self.DoesNotExist:
+    #         return False
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+admin.site.register(Question)
--- djangosite/testwebsite/polls/views.py
+++ djangosite/testwebsite/polls/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+# Modelling imports
+from .models import Question, Choice
+
+# Show the 5 most recent polls
 def index(request):
-    return HttpResponse("This is super cool! Haha!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+def test(request):
+    return HttpResponse("You just went to /test")
+
+# The information regarding the 2nd bU
+def detail(request, question_id):
+    q = Question.objects.filter(pk=question_id)
+    c = Choice.objects.filter(question=q.get().id)
+    context = {
+        "question": q.get() if q.exists() else False,
+        "choices": c if c.exists() else False,
+        "question_id": question_id
+    }
+    return render(request, "polls/detail.html", context)
+
+    return HttpResponse("You're looking at question %s." % question_id)
+
+# Displaying the results for the 2nd bU poll
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+# Voting for 2nd bU poll
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
